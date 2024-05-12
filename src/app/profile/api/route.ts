a7add67939fbdd25ac8cc5a8a4764c773d7febeb
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-05-12 19:38:47 +0530

    cookies (with inbuilt request method)

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,24 @@
-export async function GET(){
-    return new Response("Profile API Data")
+import { headers } from "next/headers"
+import { NextRequest } from "next/server"
+
+
+export async function GET(request:NextRequest){
+    const requestHeaders= new Headers(request.headers)
+    const headerList=headers()
+    // const requestHeaders= new Headers(request.headers)
+    // const headerList=headers()
+    // Both works same
+
+    const theme=request.cookies.get("theme")
+    console.log(theme)
+
+    console.log(requestHeaders.get("Authorization"))
+    console.log(headerList.get("Authorization"))
+    return new Response("<h1>Profile API Data</h1>",{
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark"
+        }
+    })
 }
+
