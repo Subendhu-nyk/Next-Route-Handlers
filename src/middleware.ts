678322806e7c86880dfb28be325a647f69dfa62a
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-06-11 10:45:18 +0530

    middleware(setting cookies)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,29 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware (request:NextRequest){
-  return (
-    NextResponse.redirect(new URL("/",request.url))
-  )
+//   return (
+//     NextResponse.redirect(new URL("/",request.url))
+//   )
+// redirection
+// if(request.nextUrl.pathname==='/profile'){
+//     return (NextResponse.redirect(new URL('/hello',request.url)))
+// }
+
+// url rewrite (it will redirected to hello but the url will be same that is profile)
+  // if (request.nextUrl.pathname === "/profile") {
+  //   return NextResponse.rewrite(new URL("/hello", request.nextUrl));
+  // }
+
+  const response=NextResponse.next()
+  const themePreference=request.cookies.get('theme')
+  if(!themePreference){
+    response.cookies.set("theme","dark")
+  }
+  return response;
+
+
 }
 
-export const config={
-    matcher:"/profile",
-    }
+// export const config={
+//     matcher:"/profile",
+//     }
